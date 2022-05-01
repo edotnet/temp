08bd2286889b98af982d01de6b26afee65b4a3bb
Jordi Garcia <jordi.garcia@lambdaloopers.com>
2022-05-01 11:44:20 +0200

    fix category filter

--- src/modules/drug-interaction/CategoryAutocomplete.jsx
+++ src/modules/drug-interaction/CategoryAutocomplete.jsx
@@ -1,30 +1,23 @@
 import { useApiCall } from "../../infrastructure/hooks/useApiCall";
 import { Autocomplete } from "../../infrastructure/components/Autocomplete";
-import { useState } from "react";
 
 export const CategoryAutocomplete = ({onChange, onEmpty}) => {
   const url = `drugbank/category/query/`;
   const {loading, data, fetch} = useApiCall(url, null, null, false);
-  const [label, setLabel] = useState("Without filter");
   const executeSearch = (search) => {
-    if (search.length) {
-      setLabel('');
-    }else {
-      setLabel('Without filter')
-    }
     if (search.length > 3) {
       fetch(`${url}${search}?page=0`, 'GET')
     }
   }
 
   const options = data ? data.items.map(item => ({
-      id: item.drugbank_id,
-      label: item.name
-    })) : [];
+    id: item.drugbank_id,
+    label: item.name
+  })) : [];
 
   return (
     <Autocomplete
-      label={label}
+      label="Drug family"
       onChange={newValue => onChange(newValue.id)}
       onInputChange={newValue => executeSearch(newValue)}
       onEmpty={onEmpty}
