b7d3c29e22ae909c3394487a0748777ceb7657c0
Jordi Garcia <jordi.garcia@lambdaloopers.com>
2022-05-01 11:25:31 +0200

    fix key error

--- src/modules/dashboard/DTI.jsx
+++ src/modules/dashboard/DTI.jsx
@@ -64,7 +64,7 @@ export const DTI = () => {
                   '\n)'
               }}>
                 {data && data.map(el => (
-                  <Box pt={3.3}>
+                  <Box pt={3.3} key={el.value}>
                     <Graph width={el.value}/>
                   </Box>
                 ))}
