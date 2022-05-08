b3b76359b2b8ae3c11ce830cbd8450a5417fecaf
Jordi Garcia <jordi.garcia@lambdaloopers.com>
2022-05-08 19:51:12 +0200

    drawDot

--- src/modules/dashboard/predictive-world/PredictiveWorld.jsx
+++ src/modules/dashboard/predictive-world/PredictiveWorld.jsx
@@ -5,18 +5,12 @@ import { Slider } from "@mui/material";
 export const PredictiveWorld = () => {
   let ctx = null;
   const canvas = useRef()
-  const drawLine = (info, style = {}) => {
-    const { x, y, x1, y1 } = info;
-    const { color = 'black', width = 1 } = style;
+  const bars = 8;
+  const radius = 130;
+  const graus = 45;
 
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x1, y1);
-    ctx.strokeStyle = color;
-    ctx.lineWidth = width;
-    ctx.stroke();
-  }
-  const draw_rectangle = (x,y,w,h,deg) => {
+
+  const drawLine = (x,y,w,h,deg) => {
     ctx.save();
     ctx.translate(x, y);
     ctx.rotate(degrees_to_radians(deg+90));
@@ -25,6 +19,16 @@ export const PredictiveWorld = () => {
     ctx.restore();
   }
 
+  const drawDot = (lineDeg, value) => {
+    ctx.save();
+    var x = radius * Math.cos(degrees_to_radians(lineDeg * graus));
+    var y = radius * Math.sin(degrees_to_radians(lineDeg * graus));
+    ctx.translate(x+250, y+250);
+    ctx.rotate(-degrees_to_radians(lineDeg*graus));
+    ctx.fillStyle = "#209ff4";
+    ctx.fillRect(-5, value, 10, 10);
+    ctx.restore();
+  }
 
   const degrees_to_radians = (degrees) => {
     return degrees * Math.PI / 180;
@@ -42,22 +46,23 @@ export const PredictiveWorld = () => {
     ctx = canvasEle.getContext("2d");
   }, []);
   useEffect(() => {
-    /*drawLine({ x: 0, y: 0, x1: 300, y1: 300 }, { color: '#209ff4', width: 2, opacity: 0.24 });
-    drawLine({ x: 300, y: 0, x1: 0, y1: 300 }, { color: '#209ff4', width: 2, opacity: 0.24 });*/
 
     var bars = 8;
     var radius = 130;
     for(var i = 0; i < bars; i++) {
-      var x = radius * Math.cos(degrees_to_radians(i * 45));
-      var y = radius * Math.sin(degrees_to_radians(i * 45));
-      draw_rectangle(x + 250, y + 250, 1, 120, i * 45);
+      var x = radius * Math.cos(degrees_to_radians(i * graus));
+      var y = radius * Math.sin(degrees_to_radians(i * graus));
+      drawLine(x + 250, y + 250, 1, 120, i * graus);
     }
 
-    ctx.fillRect(150,150,10,10);
-    ctx.fillRect(180,180,10,10);
-
-    ctx.fillRect(360,360,10,10);
-    ctx.fillRect(320,320,10,10);
+    drawDot(1, -50)
+    drawDot(2, -50)
+    drawDot(3, 20)
+    drawDot(4, 20)
+    drawDot(5, 20)
+    drawDot(6, 20)
+    drawDot(7, 20)
+    drawDot(8, 10)
   }, []);
 
   return (
