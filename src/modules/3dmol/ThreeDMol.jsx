7c615c6507f0b409a1c4309ac49a621e24d5b08e
Jordi Garcia <jordi.garcia@lambdaloopers.com>
2022-05-17 13:28:48 +0200

    Add modal

--- src/modules/3dmol/ThreeDMol.jsx
+++ src/modules/3dmol/ThreeDMol.jsx
@@ -1,15 +1,17 @@
-import { useEffect, useRef } from 'react';
+import { memo, useEffect, useRef, useState } from 'react';
 import { useDashboardContext } from "../dashboard/context/useDashboarContext";
 import $ from 'jquery';
 import $3Dmol from '3dmol';
 import { useProtein } from "./useProtein";
+import { ModalPaper } from "../../infrastructure/components/ModalPaper";
+import { Box, IconButton } from "@mui/material";
+import { Close } from "@mui/icons-material";
 
 export const ThreeDMol = () => {
   const ref = useRef();
   const {state} = useDashboardContext()
-  console.log('protein', state.protein)
+  const [isOpen, open] = useState(true);
   useEffect(() => {
-    console.log($3Dmol.viewers)
     if (state.protein) {
       const pdb = state.protein.pdb_ids[0]
       const viewer = $3Dmol.viewers[0];
@@ -17,14 +19,27 @@ export const ThreeDMol = () => {
       $3Dmol.download(`pdb:${pdb}`, viewer,{format: 'pdb', colorschema: 'spectral'}, () => {
         viewer.zoomTo();
         viewer.render();
+        open(true)
       })
     }
   }, [state.protein])
 
+  const close = () => {
+    open(false);
+  }
+
   return (
-    <div style={{height: '400px', width: '400px', position: 'relative'}}
-         className="viewer_3Dmoljs"
-         data-backgroundcolor="#f5f6fb"
-         data-style="stick"/>
+    <Box sx={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', display: isOpen ? 'block' : 'none'}}>
+      <ModalPaper>
+        <IconButton sx={{position: "absolute", top: 0, right: 0, zIndex: 10}} size="large" onClick={close}>
+          <Close/>
+        </IconButton>
+        <div style={{height: '400px', width: '400px', position: 'relative'}}
+             className="viewer_3Dmoljs"
+             data-backgroundcolor="#f5f6fb"
+             data-pdb="2nbd"
+             data-style="stick"/>
+      </ModalPaper>
+    </Box>
   )
 }
