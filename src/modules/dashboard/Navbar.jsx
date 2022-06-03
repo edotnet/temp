30eb34f660baf49d1b8c0fd317445c90da7d5564
tbuddhi <tharakabuddhi@gmail.com>
2022-06-03 13:04:32 +0530

    cosmetic fixes3

--- src/modules/dashboard/Navbar.jsx
+++ src/modules/dashboard/Navbar.jsx
@@ -1,9 +1,10 @@
 import { Box, Grid } from "@mui/material";
-import moleculeimg from "../../assets/img/group-11.png";
+// import moleculeimg from "../../assets/img/group-11.png";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import { useLocation, useNavigate } from "react-router-dom";
 import Logo from '../../assets/svg/logo.svg';
+import './NavBar.scss'
 
 export const Navbar = () => {
   const navigate = useNavigate();
@@ -14,23 +15,23 @@ export const Navbar = () => {
   };
   return <Box>
     <Box component="div" className="dashboardcircle">
-      <Box component="img" alt="image" className="moleculeimg" src={moleculeimg}/>
+      {/* <Box component="img" alt="image" className="moleculeimg" src={moleculeimg}/> */}
     </Box>
     <Box className="dashboardheader">
-      <Grid container spacing={2}>
-        <Grid item xs={2}>
+      <Grid container>
+        <Grid item xs={2} className='logoWrapper'>
           <Box component="img" alt="logo" className="logoresponsive"
                src={Logo}/>
         </Grid>
         <Grid item xs={10}>
           <Tabs value={location.pathname} onChange={handleChange}
-                TabIndicatorProps={{sx: {backgroundColor: "#979797", height: 2},}}>
-            <Tab value="/drugbank" label="DRUGBANK" sx={{color: "#000", fontWeight: "bold"}}/>
-            <Tab value="/categories" label="CATEGORIES" sx={{color: "#000", fontWeight: "bold"}}/>
-            <Tab value="/dashboard" label="DRUG INTERACTION" sx={{color: "#000", fontWeight: "bold"}}/>
-            <Tab value="/dti" label="DTI" sx={{color: "#000", fontWeight: "bold"}}/>
-            <Tab value="/engine" label="Search engine" sx={{color: "#000", fontWeight: "bold"}}/>
-            <Tab value="/surface" label="Surface" sx={{color: "#000", fontWeight: "bold"}}/>
+                TabIndicatorProps={{sx: {backgroundColor: "#979797", height: 2},}} className="nav-TabList">
+            <Tab value="/drugbank" label="DRUGBANK" />
+            <Tab value="/categories" label="CATEGORIES"/>
+            <Tab value="/dashboard" label="DRUG INTERACTION" />
+            <Tab value="/dti" label="DTI" />
+            <Tab value="/engine" label="Search engine" />
+            <Tab value="/surface" label="Surface" />
           </Tabs>
         </Grid>
       </Grid>
