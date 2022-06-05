beaf3d956ef20a429e5a14c657367570008ff468
jordi <jordi@prepaire.com>
2022-06-05 09:51:33 +0200

    Changing api

--- src/config/Consts.js
+++ src/config/Consts.js
@@ -1,3 +1,15 @@
 export const Consts = {
-  API_URL: process.env.REACT_APP_API_URL || `${window.location.origin}:2053`
+  BACKEND_API_URL: process.env.REACT_APP_BACKEND_API_URL,
+  API_URL: process.env.REACT_APP_API_URL,
 }
+
+const useApi = window.location.search.includes("isApi=true") ?
+    Consts.API_URL : Consts.BACKEND_API_URL;
+
+export const Endpoints = {
+  drugbank: {
+    query: `${useApi}/drugbank/query/`,
+    targets: `${useApi}/drugbank/target/query/`,
+    categories: `${useApi}/drugbank/category/query/`,
+  },
+};
