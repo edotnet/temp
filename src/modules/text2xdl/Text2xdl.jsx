782d9fcfa52a11466bfbe98371cfad933b18ae79
tbuddhi <tharakabuddhi@gmail.com>
2022-06-08 00:36:13 +0530

    Implement xml formatter and copy buttons

--- src/modules/text2xdl/Text2xdl.jsx
+++ src/modules/text2xdl/Text2xdl.jsx
@@ -1,13 +1,16 @@
 import { Box, TextField } from "@mui/material";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useApiCall } from "../../infrastructure/hooks/useApiCall";
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import beautify from "xml-beautifier";
 import CircularProgress from '@mui/material/CircularProgress';
-
+import IconButton from '@mui/material/IconButton';
+import ContentCopy from '@mui/icons-material/ContentCopy';
 
 export const Text2xdlFeature = () => {
+  const xdlText = useRef(null);
+  // const [copySuccess, setCopySuccess] = useState('')
   const [text, setText] = useState('');
   const [Loadingresult, setLoadingresult] = useState(false);
   const url = `https://api.prepaire.com/text-to-xdl`;
@@ -26,6 +29,14 @@ export const Text2xdlFeature = () => {
     const xml = beautify(value);
     return xml;
   }
+
+  // const copyToClipboard = (e) => {
+  //   xdlText.current.select();
+  //   xdlText.current.document.execCmd('copy');
+  //   e.target.focus();
+  //   setCopySuccess('Copied!');
+  // }
+
   return (
     // <Box>
     //   <TextField
@@ -42,12 +53,18 @@ export const Text2xdlFeature = () => {
     // </Box>
     <div className="searchEngine-xdl">
       <h2>Upload Text2XDL</h2>
+      {/* {copySuccess} */}
       <Grid container spacing={2}>
         <Grid item xs={4}>
           <div className="text">
             <div className="translate-block">
               <h4>2XDL text</h4>
-              <TextField id="style-2" variant="standard" InputProps={{ disableUnderline: true}} className="area" fullWidth multiline rows={15} value={text} onChange={e => setText(e.target.value)} />
+              <Box sx={{position: 'absolute', right: 40, top: 13}}>
+                <IconButton color="primary" aria-label="copy to clipboard" component="span">
+                  <ContentCopy />
+                </IconButton>
+              </Box>
+              <TextField id="style-2" ref={xdlText} variant="standard" InputProps={{ disableUnderline: true}} className="area" fullWidth multiline rows={15} value={text} onChange={e => setText(e.target.value)} />
               <Button onClick={onRun} disabled={!text.length} sx={{my:2}} variant="outlined">Translate To Synthesis XDL Code</Button>
             </div>
           </div>
@@ -58,16 +75,28 @@ export const Text2xdlFeature = () => {
               <div className="result" id="style-2">
                 <h4 className="heading">Drug Synthesis Translated to XDL Code</h4>
                 <Grid container spacing={2}>
-                  <Grid item xs={6}>
+                  <Grid item xs={6} sx={{position: 'relative'}}>
                     <h4>Synthesis Process</h4>
+                    <Box sx={{ position: 'absolute', right: 10, top: 30}}>
+                      <IconButton color="primary" aria-label="copy to clipboard" component="span">
+                        <ContentCopy />
+                      </IconButton>
+                    </Box>
                     <div className="process" id="style-2">
                       {data && data.result ? NewlineText(data.result.text) : ''}
                     </div>
                   </Grid>
-                  <Grid item xs={6}>
+                  <Grid item xs={6} sx={{position: 'relative'}} >
+                    <h4>Synthesis XDL</h4>
+                    <Box sx={{position: 'absolute', right: 10, top: 30}}>
+                      <IconButton color="primary" aria-label="copy to clipboard" component="span">
+                        <ContentCopy />
+                      </IconButton>
+                    </Box>
                     <div className="synthesis" id="style-2">
-                      <h4>Synthesis XDL</h4>
-                      {data && data.result ? prettyformat(data.result.xml)  : ''}
+                      <pre>
+                        {data && data.result ? prettyformat(data.result.xml)  : ''}
+                      </pre>
                     </div>
                   </Grid>
                 </Grid>
