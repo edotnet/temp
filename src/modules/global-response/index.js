ece8048162e4617ca012ab8dfca734fb4ceb31e9
AndriyYurchuk <paltashka50@gmail.com>
2022-12-05 02:29:15 +0200

    change numberWithComm function for safari

--- src/modules/global-response/index.js
+++ src/modules/global-response/index.js
@@ -47,5 +47,8 @@ export const getRoundSize = cases => {
   return 30
 }
 
-export const numberWithCommas = num =>
-  num.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')
+export const numberWithCommas = num => {
+  const parts = num.toString().split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return parts.join(".");
+} 
